33e45494c49f42a32c8bc42f092ddcd87f152466
mohamed khairy <mohamed.khairy@devexy.com>
2021-03-17 11:20:28 +0200

    adding IOSThemeConfiguration.dart

---
+++ lib/IOSThemeConfiguration.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+class IOSThemeConfigurations {
+  Color primaryColor;
+  Color primaryFontColor;
+  Color secondaryColor;
+  Color secondaryFontColor;
+  Color strokeColor;
+  Color buttonColor;
+  Color buttonFontColor;
+  Color titleFontColor;
+  Color backgroundColor;
+  Color placeholderColor;
+  int primaryFontSize;
+  int secondaryFontSize;
+  int strokeThinckness;
+  int inputsCornerRadius;
+  int buttonFontSize;
+  int titleFontSize;
+
+  IOSThemeConfigurations({
+    this.primaryColor,
+    this.primaryFontColor,
+    this.secondaryColor,
+    this.secondaryFontColor,
+    this.strokeColor,
+    this.buttonColor,
+    this.buttonFontColor,
+    this.titleFontColor,
+    this.backgroundColor,
+    this.placeholderColor,
+    this.primaryFontSize,
+    this.secondaryFontSize,
+    this.strokeThinckness,
+    this.inputsCornerRadius,
+    this.buttonFontSize,
+    this.titleFontSize,
+  });
+}
