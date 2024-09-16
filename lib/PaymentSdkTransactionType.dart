e4ada12e867894db590bd91d42aa5af2819e4b4a
Muhammad Alkady <kady.muhammad@gmail.com>
2024-09-16 20:32:31 +0300

    chore: document PaymentSdkTransactionType
    - add documentation for PaymentSdkTransactionType class

--- lib/PaymentSdkTransactionType.dart
+++ lib/PaymentSdkTransactionType.dart
@@ -1,12 +1,29 @@
-enum PaymentSdkTransactionType { SALE, AUTH, REGISTER }
+/// Enum representing different types of payment transactions.
+enum PaymentSdkTransactionType {
+  /// Represents a sale transaction.
+  SALE,
 
+  /// Represents an authorization transaction.
+  AUTH,
+
+  /// Represents a register transaction.
+  REGISTER
+}
+
+/// Extension on [PaymentSdkTransactionType] to provide additional functionality.
 extension PaymentSdkTransactionTypeExtension on PaymentSdkTransactionType {
+  /// Gets the name of the transaction type as a string.
+  ///
+  /// Returns:
+  /// - "auth" for [PaymentSdkTransactionType.AUTH]
+  /// - "register" for [PaymentSdkTransactionType.REGISTER]
+  /// - "sale" for [PaymentSdkTransactionType.SALE] (default)
   String get name {
     switch (this) {
       case PaymentSdkTransactionType.AUTH:
         return "auth";
       case PaymentSdkTransactionType.REGISTER:
-        return "Register";
+        return "register";
       default:
         return "sale";
     }
